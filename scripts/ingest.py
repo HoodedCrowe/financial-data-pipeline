6b54fa17eba46ba36a0132b98ce64fe18ef51894
Jonathan Crowe <git@jcrowe.dev>
2026-01-12 18:26:04 +0100

    database inserts for yahoo finance market data

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -2,6 +2,8 @@ import argparse
 from enum import Enum
 from src.models import DataSource
 from src.ingestion.base import YahooFinanceSource
+from src.database import MarketDataRepository
+from src.database import IngestionLogRepository
 from datetime import datetime, timedelta
 
 parser = argparse.ArgumentParser("Ingest")
@@ -22,8 +24,11 @@ end_date = datetime.now()
 start_date = end_date - timedelta(days=days)
 
 yahoo_source = YahooFinanceSource()
+market_data_repository = MarketDataRepository()
+ingestion_log_repository = IngestionLogRepository()
 
 for s in symbol_list:
-    df = yahoo_source.fetch_ohlcv(symbol=s, start_date=start_date, end_date=end_date)
-    print(df)
+    ohlcv_records = yahoo_source.fetch_ohlcv(symbol=s, start_date=start_date, end_date=end_date)
+    market_data_repository.insert_records(records=ohlcv_records)
+    ingestion_log_repository.log_run(source=source, symbol=s)
     break
